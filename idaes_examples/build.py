42f4f154bf9169797753f497560bf923b55ef37f
Dan Gunter <dkgunter@lbl.gov>
2022-10-28 09:04:31 -0700

    better handling of links

--- idaes_examples/build.py
+++ idaes_examples/build.py
@@ -125,8 +125,10 @@ exclude_tags = {
 
 # notebook filenames, e.g. in markdown links
 # NOTE: assume no spaces in filenames
-nb_file_pat = re.compile(r"([a-zA-Z0-9_\-:.+]+)_src\.ipynb")
-nb_file_repl = r"\1_doc.ipynb"
+nb_file_pat = re.compile(f"([a-zA-Z0-9_\\-:.+]+){src_suffix}\\.ipynb")
+nb_file_subs = {e.value: f"\\1_{e.value}.ipynb" for e in Ext if e != Ext.DOC}
+# For MyST, replace .ipynb with .md in the 'doc' version
+nb_file_subs[Ext.DOC.value] = f"\\1_{Ext.DOC.value}.md"
 
 
 def _preprocess(nb_path: Path, **kwargs):
@@ -134,49 +136,55 @@ def _preprocess(nb_path: Path, **kwargs):
     t0 = time.time()
 
     # Load input file
-    with nb_path.open('r', encoding='utf-8') as nb_file:
+    with nb_path.open("r", encoding="utf-8") as nb_file:
         nb = json.load(nb_file)
 
-    # Find cells to exclude
+    # Get cells to exclude, also ones with notebook xrefs
     had_tag = set()  # if tag occurred at all
     exclude_cells = {n: [] for n in exclude_tags}
+    xref_cells = {}  # {cell-index: [list of line indexes]}
     for cell_index, cell in enumerate(nb[NB_CELLS]):
+        # Get tags for cell
         cell_tags = set(cell["metadata"].get("tags", []))
         for c in cell_tags:
             try:
                 had_tag.add(Tags(c))
             except ValueError:  # not in Tags enum
                 pass
-        # Add cell to each notebook in which it should not be excluded
+        # Potentially add to lists of excluded cells
         for name, ex_tags in exclude_tags.items():
             if cell_tags & ex_tags:
                 # add in reverse order to make delete easier
                 exclude_cells[name].insert(0, cell_index)
+        # Look for (and save) lines with cross references
+        xref_lines = [i for i, line in enumerate(cell["source"])
+                      if nb_file_pat.search(line)]
+        if xref_lines:
+            xref_cells[cell_index] = xref_lines
 
     # Write output files
+
     nb_names = [Ext.TEST.value, Ext.DOC.value]
     is_tutorial = had_tag & {Tags.EX, Tags.SOL}
     if is_tutorial:
         nb_names.extend([Ext.EX.value, Ext.SOL.value])
+
     for name in nb_names:
-        # Generate notebook copy
         nb_copy = nb.copy()
         nb_copy[NB_CELLS] = nb[NB_CELLS].copy()
+        # Fix (any) cross-references to use current file extension ('name')
+        saved_source = []  # save (index, orig-source) for all changed cells
+        for cell_index, cell in enumerate(nb_copy[NB_CELLS]):
+            if cell_index not in xref_cells or cell_index in exclude_cells[name]:
+                continue
+            cs = cell["source"]  # alias
+            saved_source.append((cell_index, cs.copy()))  # save orig
+            for line_index in xref_cells[cell_index]:
+                cs[line_index] = nb_file_pat.sub(nb_file_subs[name], cs[line_index])
+        # Delete excluded cells
         for index in exclude_cells[name]:
             del nb_copy[NB_CELLS][index]  # indexes are in reverse order
-        # replace occurrences of notebook files with _doc extension so xrefs work
-        if name == Ext.DOC.value:
-            for cell in nb_copy[NB_CELLS]:
-                cell_replace = {}
-                for i, line in enumerate(cell["source"]):
-                    if nb_file_pat.search(line):
-                        cell_replace[i] = nb_file_pat.sub(nb_file_repl, line)
-                if cell_replace:
-                    for i in cell_replace:
-                        print(f"@@ replace cell source[{i}]: {cell_replace[i]} ")
-                        cell["source"][i] = cell_replace[i]
-                    print(f"@@ {name} cell source: {cell['source']}")
-        # Generate output file for copy
+        # Generate output file
         output_dir = nb_path.parent
         base_name = nb_path.stem[:-src_suffix_len]
         output_name = f"{base_name}_{name}.ipynb"
@@ -184,6 +192,9 @@ def _preprocess(nb_path: Path, **kwargs):
         _log.debug(f"Generate '{name}' file: {nbcopy_path}")
         with nbcopy_path.open("w") as nbcopy_file:
             json.dump(nb_copy, nbcopy_file)
+        # Restore modified sources
+        for i, s in saved_source:
+            nb[NB_CELLS][i]["source"] = s
 
     dur = time.time() - t0
     _log.info(f"Prepocessed notebook {nb_path} in {dur:.2f} seconds")
@@ -193,9 +204,9 @@ def _preprocess(nb_path: Path, **kwargs):
 # Clean
 # -------------
 
+
 def clean(srcdir=None):
-    src_path = allow_repo_root(Path(srcdir), main)
-    src_path /= NB_ROOT
+    src_path = allow_repo_root(Path(srcdir), main) / NB_ROOT
     toc = read_toc(src_path)
     find_notebooks(src_path, toc, _clean)
 
@@ -216,6 +227,7 @@ def _clean(nb_path: Path, **kwargs):
 #  Jupyterbook
 # -------------
 
+
 def jupyterbook(srcdir=None):
     path = allow_repo_root(Path(srcdir), main)
     path /= NB_ROOT
@@ -230,7 +242,8 @@ def jupyterbook(srcdir=None):
 
 
 def view_docs(srcdir=None):
-    docs_path = Path(srcdir) / NB_ROOT / "_build" / "html"
+    src_path = allow_repo_root(Path(srcdir), main) / NB_ROOT
+    docs_path = src_path / "_build" / "html"
     if not docs_path.is_dir():
         raise FileNotFoundError(f"Could not find directory: {docs_path}")
 
@@ -242,6 +255,7 @@ def view_docs(srcdir=None):
 # -------------
 #  Commandline
 # -------------
+
 
 class Commands:
     @classmethod
@@ -307,7 +321,10 @@ def main():
         )
         add_vb(subp[name], dest=f"vb_{name}")
     subp["build"].add_argument(
-        "--no-pre", action="store_true", help="skip pre-processing", default=False,
+        "--no-pre",
+        action="store_true",
+        help="skip pre-processing",
+        default=False,
     )
     args = p.parse_args()
     subvb = getattr(args, f"vb_{args.command}")
