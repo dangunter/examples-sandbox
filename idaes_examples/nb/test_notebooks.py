dc8c0aedb30861569f4c54afc23e22c58e029f69
Dan Gunter <dkgunter@lbl.gov>
2022-11-04 06:36:12 -0700

    test nb syntax

--- idaes_examples/nb/test_notebooks.py
+++ idaes_examples/nb/test_notebooks.py
@@ -1,10 +1,17 @@
 """
 Tests for notebooks (without running them)
 """
+# stdlib
+import os
 from pathlib import Path
+import subprocess
 from typing import List
+
+# third-party
 import pytest
-from idaes_examples.util import read_toc, allow_repo_root, NB_ROOT, find_notebooks
+
+# package
+from idaes_examples.common import read_toc, find_notebooks
 
 
 # -------------------
@@ -46,3 +53,16 @@ def test_smoke(notebooks: List[Path]):
         assert nb_path.stat().st_size > 0
 
 
+# Use 'black --check' to test whether syntax is OK in Jupyter notebooks.
+# This requires that black[jupyter] has been installed.
+def test_black():
+    working_dir = Path(__file__).parent
+    command = ["black", "--check", "--include", ".*_src\\.ipynb", str(working_dir)]
+    proc = subprocess.Popen(command, stderr=subprocess.PIPE)
+    _, stderr_data = proc.communicate(timeout=20)
+    if proc.returncode != 0:
+        # print out errors for pytest's captured stdout
+        for line in stderr_data.decode("utf-8").split(os.linesep):
+            if line.startswith("error"):
+                print(line)
+        assert False, "Black format check failed for *_src.ipynb"
